5b98e41049a35495420a62d66957463881d32911
akibo567 <akibo567@live.jp>
2023-03-29 15:55:46 +0900

    バックエンドシステムとの通信を実装

--- src/screen/search.js
+++ src/screen/search.js
@@ -1,47 +1,36 @@
 
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import ShopList from "../components/shop_list"
+import {API_ENDPOINT} from "../settings"
 
 function Search() {
   const [Latitude_Data, Set_Latitude_Data] = useState();
   const [Longitude_Data, Set_Longitude_Data] = useState();
 
-  const [Searched_List, Set_Searched_List] = useState([{}]);
+  const [Searched_List, Set_Searched_List] = useState();
 
+  const axios_instance = axios.create({
+    baseURL: API_ENDPOINT,
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    timeout: 2000,
+  });
 
   const Search_Button_Onclick = () => {
-    Set_Searched_List(
-      [
-        {
-          name:"ダミー1",
-          acccess:"HOGEから徒歩一分",
-          thumb_img:"hoge.jpg",
-          location:"A町B市",
-          business_hour:"8:00~18:00",
-        },
-        {
-          name:"ダミー2",
-          acccess:"HOGEから徒歩一分",
-          thumb_img:"hoge.jpg",
-          location:"A町B市",
-          business_hour:"8:00~18:00",
-        },
-        {
-          name:"ダミー3",
-          acccess:"HOGEから徒歩一分",
-          thumb_img:"hoge.jpg",
-          location:"A町B市",
-          business_hour:"8:00~18:00",
-        },
-        {
-          name:"ダミー4",
-          acccess:"HOGEから徒歩一分",
-          thumb_img:"fuga.jpg",
-          location:"市",
-          business_hour:"18:00",
-        }
-      ],
-    );
+    axios_instance
+    .post('/search_shop', {
+      Latitude: Latitude_Data,
+      Longitude: Longitude_Data
+    })
+    .then(function (response) {
+      Set_Searched_List(response.data);
+    })
+    .catch(function () {
+      alert("通信エラー");
+    });
   };
   
   useEffect(() => {
@@ -63,9 +52,9 @@ function Search() {
             }}
             className="load_button"
       >検索</button>
-     <ShopList
+     {Searched_List && <ShopList
         shop_items={Searched_List}
-     />
+     />}
     </div>
   );
 }
---
+++ src/settings.js
@@ -0,0 +1,3 @@
+const API_ENDPOINT = "http://127.0.0.1:5000";
+
+export {API_ENDPOINT};
