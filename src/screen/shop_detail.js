31f5434055e8778c2aa332f8b3873d5705ba55e4
akibo567 <akibo567@live.jp>
2023-03-30 15:31:36 +0900

    shop_detailに直接アクセスした際の不具合を修正

--- src/screen/shop_detail.js
+++ src/screen/shop_detail.js
@@ -6,26 +6,29 @@ function ShopDetail(props) {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const [Shop_Item, Set_Shop_Item] = useState(location.state.shopitem);
+  const [Shop_Item, Set_Shop_Item] = useState(null);
 
 
- // const [selectId, setSelectId] 
- // = useState(location.state);
-  
   useEffect(() => {
     //データがない場合、TOPにリダイレクト
     if(!location.state){
       navigate('/');
+    }else{
+      Set_Shop_Item(location.state.shopitem);
     }
   });
 
   return (
-      <div className="Detail_Screen">
-      <p>店舗名称:{Shop_Item.name}</p>
-      <p>住所:{Shop_Item.location}</p>
-      <p>営業時間:{Shop_Item.business_hour}</p>
-      <p><img src={Shop_Item.thumb_img} alt=""/></p>
-      </div>
+    <div class="shop_detail">
+      {Shop_Item &&
+        <div className="Detail_Screen">
+        <p>店舗名称:{Shop_Item.name}</p>
+        <p>住所:{Shop_Item.location}</p>
+        <p>営業時間:{Shop_Item.business_hour}</p>
+        <p><img src={Shop_Item.thumb_img} alt=""/></p>
+        </div>
+      }
+    </div>
   );
 }
 
